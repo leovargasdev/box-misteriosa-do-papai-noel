7c8efc353d99c64f37d08ffb18cdc0aacd5c2b64
Leonardo Vargas <leu1607@hotmail.com>
2023-12-18 08:12:19 -0300

    criando projeto

---
+++ README.md
@@ -0,0 +1,36 @@
+<h1 align="center">
+  Box Natalina
+</h1>
+
+<p align="center">
+  <img src=".github/preview.png" width="100%" />
+</p>
+
+## 💻 Projeto
+
+Neste tutorial, vamos criar o site oficial do Papai Noel utilizando _HTML_, _CSS_ e _JavaScript_! 🎅✨ Descubra passo a passo como desenvolvemos uma encantadora landing page que destaca a venda da misteriosa box do Papai Noel. 🎁💻
+
+## 🚀 Tecnologias
+
+- HTML
+- CSS
+- JavaScript
+- [AOS Animate](https://michalsnik.github.io/aos/)
+- [Particles.js](https://particles.js.org/)
+
+## 📔 Conhecimentos abordados
+
+- [x] Uso semântico do HTML
+- [x] Váriaveis do css no `:root`
+- [x] Animações com a biblioteca [AOS](https://michalsnik.github.io/aos/)
+- [x] Uso do CSS Flexbox
+- [x] Efeitos com a propriedade `transform` do css
+- [x] Manipulação de arquivos de audio
+- [x] Animações com a biblioteca [particles.js](https://particles.js.org/)
+
+
+## 📺 Tutorial no Youtube
+
+<a href="https://www.youtube.com/watch?v=pb4lZLLObUM" target="_blank">
+ <img src="https://img.youtube.com/vi/pb4lZLLObUM/maxresdefault.jpg" width="100%" height="auto" />
+</a>
---
+++ scripts/confetti-effect.js
@@ -0,0 +1,134 @@
+const CONFIG_CONFETTI = {
+  "fullScreen": {
+    "enable": false,
+    "zIndex": 1
+  },
+  "particles": {
+    "number": {
+      "value": 0
+    },
+    "color": {
+      "value": [
+        "#00FFFC",
+        "#FC00FF",
+        "#fffc00"
+      ]
+    },
+    "shape": {
+      "type": [
+        "circle",
+        "square"
+      ],
+      "options": {}
+    },
+    "opacity": {
+      "value": 1,
+      "animation": {
+        "enable": true,
+        "minimumValue": 0,
+        "speed": 2,
+        "startValue": "max",
+        "destroy": "min"
+      }
+    },
+    "size": {
+      "value": 6,
+      "random": {
+        "enable": true,
+        "minimumValue": 2
+      }
+    },
+    "links": {
+      "enable": false
+    },
+    "life": {
+      "duration": {
+        "sync": true,
+        "value": 5
+      },
+      "count": 1
+    },
+    "move": {
+      "enable": true,
+      "gravity": {
+        "enable": true,
+        "acceleration": 10
+      },
+      "speed": {
+        "min": 10,
+        "max": 50
+      },
+      "decay": 0.1,
+      "direction": "none",
+      "straight": false,
+      "outModes": {
+        "default": "destroy",
+        "top": "none"
+      }
+    },
+    "rotate": {
+      "value": {
+        "min": 0,
+        "max": 360
+      },
+      "direction": "random",
+      "move": true,
+      "animation": {
+        "enable": true,
+        "speed": 200
+      }
+    },
+    "tilt": {
+      "direction": "random",
+      "enable": true,
+      "move": true,
+      "value": {
+        "min": 0,
+        "max": 360
+      },
+      "animation": {
+        "enable": true,
+        "speed": 600
+      }
+    },
+    "roll": {
+      "darken": {
+        "enable": true,
+        "value": 25
+      },
+      "enable": true,
+      "speed": {
+        "min": 15,
+        "max": 25
+      }
+    },
+    "wobble": {
+      "distance": 30,
+      "enable": true,
+      "move": true,
+      "speed": {
+        "min": -15,
+        "max": 15
+      }
+    }
+  },
+  "emitters": {
+    "life": {
+      "count": 1,
+      "duration": 1,
+      "delay": 0.4
+    },
+    "rate": {
+      "delay": 1,
+      "quantity": 150
+    },
+    "size": {
+      "width": 0,
+      "height": 0
+    }
+  }
+}
+
+function handleConfettiEffect() {
+  tsParticles.load("container-confetti", CONFIG_CONFETTI)
+}
